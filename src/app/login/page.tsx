5f230f164f28787ab6a78c786c511e51e9bf6b4c
Moubardan <mouhssine.oubardan@hotmail.com>
2026-04-13 16:56:38 +0100

    fix uuid issue

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default async function LoginPage() {
     const session = await auth();
 
-    if (session?.user) {
+    if (session?.user && session.accessToken && !session.error) {
         redirect("/dashboard");
     }
 
